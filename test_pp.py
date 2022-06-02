908dcbc0a64836dfb592e393825efad8b132a012
gwyxjtu <867718012@qq.com>
2022-06-02 21:26:53 +0800

    bus_csv

--- test_pp.py
+++ test_pp.py
@@ -8,8 +8,9 @@ print(net.bus)
 print(net.line)
 print(net.trafo)
 
-
+net.bus.to_csv("bus.csv")
+net.line.to_csv("line.csv")
+net.trafo.to_csv("trafo.csv")
 
 # from pandapower.plotting.plotly import simple_plotly
-
 # simple_plotly(net)
